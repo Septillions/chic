497d529fc0b4c2b4e74dc77b968b943188491541
Septillions <eolinker@gmail.com>
2019-10-27 23:25:21 +0800

    添加 admin 模块初始配置

---
+++ chic-admin/src/main/java/com/github/chic/admin/ChicAdminApplication.java
@@ -0,0 +1,15 @@
+package com.github.chic.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.github.chic.admin.mapper")
+public class ChicAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChicAdminApplication.class, args);
+    }
+
+}
