e970855c8d0222c58c1011c8e2fdd4bb8ded05ba
Septillions <eolinker@gmail.com>
2019-11-01 22:51:39 +0800

    添加 ResultCode 枚举类统一返回状态码

---
+++ chic-common/src/main/java/com/github/chic/common/component/ResultCode.java
@@ -0,0 +1,23 @@
+package com.github.chic.common.component;
+
+public enum ResultCode {
+    SUCCESS(0, "ok"),
+    UNAUTHORIZED(1001, "未登录"),
+    FORBIDDEN(1002, "无权限"),
+    FAILED(5000, "服务器异常");
+    private Integer code;
+    private String msg;
+
+    ResultCode(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
