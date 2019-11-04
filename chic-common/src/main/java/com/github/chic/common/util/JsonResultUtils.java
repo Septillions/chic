15722e4fedb73c39c774fbfc8ef908935173ab7b
Septillions <eolinker@gmail.com>
2019-11-04 23:23:34 +0800

    添加 JsonResultUtils 处理异常直接返回 Json 格式数据

---
+++ chic-common/src/main/java/com/github/chic/common/util/JsonResultUtils.java
@@ -0,0 +1,22 @@
+package com.github.chic.common.util;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.github.chic.common.component.JsonResult;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * JSON 格式返回数据工具类
+ */
+public class JsonResultUtils {
+
+    public static void responseJson(HttpServletResponse response, Integer code, String msg) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        JsonResult jsonResult = JsonResult.failed(code, msg);
+        response.getWriter().println(JSON.toJSONString(jsonResult, SerializerFeature.WriteMapNullValue));
+        response.getWriter().flush();
+    }
+}
