2788d75969f411b04853e04ac408273567fa1ebc
Septillions <597264070@qq.com>
2019-12-26 15:36:36 +0800

    添加 portal 相关组件

---
+++ chic-portal/src/main/java/com/github/chic/portal/component/handler/RestExceptionHandler.java
@@ -0,0 +1,78 @@
+package com.github.chic.portal.component.handler;
+
+import com.github.chic.common.component.JsonResult;
+import com.github.chic.common.component.ResultCode;
+import com.github.chic.common.exception.AuthException;
+import com.github.chic.common.exception.ServiceException;
+import com.github.chic.common.exception.VerifyException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 全局异常处理类
+ */
+@RestControllerAdvice
+public class RestExceptionHandler {
+    /**
+     * LOGGER
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    /**
+     * 全局异常 统一JSON格式返回
+     */
+    @ExceptionHandler(Exception.class)
+    public JsonResult exception(HttpServletRequest request, Exception e) {
+        LOGGER.error("RestExceptionHandler:", e);
+        return JsonResult.failed(e.getMessage());
+    }
+
+    /**
+     * Spring Security 未登录异常
+     */
+    @ExceptionHandler(AuthenticationException.class)
+    public JsonResult authenticationException(HttpServletRequest request, AuthenticationException e) {
+        return JsonResult.failed(ResultCode.UNAUTHORIZED.getCode(), ResultCode.UNAUTHORIZED.getMsg());
+    }
+
+    /**
+     * Spring Security 无权限异常
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    public JsonResult accessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+        return JsonResult.failed(ResultCode.FORBIDDEN.getCode(), ResultCode.FORBIDDEN.getMsg());
+    }
+
+    /**
+     * 自定义 认证相关异常
+     * errCode 1xxx 格式
+     */
+    @ExceptionHandler(AuthException.class)
+    public JsonResult authException(HttpServletRequest request, AuthException e) {
+        return JsonResult.failed(e.getErrCode(), e.getErrMsg());
+    }
+
+    /**
+     * 自定义 校验相关异常
+     * errCode 2xxx 格式
+     */
+    @ExceptionHandler(VerifyException.class)
+    public JsonResult verifyException(HttpServletRequest request, VerifyException e) {
+        return JsonResult.failed(e.getErrCode(), e.getErrMsg());
+    }
+
+    /**
+     * 自定义 业务相关异常
+     * errCode 3xxx 格式
+     */
+    @ExceptionHandler(ServiceException.class)
+    public JsonResult serviceException(HttpServletRequest request, ServiceException e) {
+        return JsonResult.failed(e.getErrCode(), e.getErrMsg());
+    }
+}
---
+++ chic-portal/src/main/java/com/github/chic/portal/util/ServletUtils.java
@@ -0,0 +1,42 @@
+package com.github.chic.portal.util;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet 工具类
+ */
+public class ServletUtils {
+    /**
+     * 获取请求 HttpServletRequest
+     */
+    public static HttpServletRequest getRequest() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return attributes != null ? attributes.getRequest() : null;
+    }
+
+    /**
+     * 获取请求 IP 地址
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
