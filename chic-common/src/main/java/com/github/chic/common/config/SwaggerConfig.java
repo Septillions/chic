033f5087632e72ac7fc1c378518d67bc571dac0d
Septillions <597264070@qq.com>
2020-04-10 15:12:41 +0800

    配置 Swagger Authorized 认证请求携带 JWT

--- chic-common/src/main/java/com/github/chic/common/config/SwaggerConfig.java
+++ chic-common/src/main/java/com/github/chic/common/config/SwaggerConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Swagger 配置类
@@ -18,17 +22,40 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket createRestApi() {
-        ApiInfo apiInfo = new ApiInfoBuilder()
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
                 .title("Chic API 文档")
                 .description("REST API")
                 .version("1.0.0")
                 .build();
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    }
+
+    private List<SecurityScheme> securitySchemes() {
+        return Collections.singletonList(new ApiKey("Auth", "Authorization", "Header"));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return Collections.singletonList(SecurityContext.builder()
+                .securityReferences(securityReferences())
+                .forPaths(PathSelectors.any())
+                .build());
+    }
+
+    private List<SecurityReference> securityReferences() {
+        return Collections.singletonList(SecurityReference.builder()
+                .reference("Auth")
+                .scopes(new AuthorizationScope[0])
+                .build());
     }
 }
