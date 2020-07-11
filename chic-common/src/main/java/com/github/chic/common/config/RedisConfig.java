9b9c5e8fe7721696f22b7a9f3328155532c89335
Septillions <597264070@qq.com>
2020-07-11 15:17:32 +0800

    修改 RedisTemplate 序列化 key 使用 StringRedisSerializer

--- chic-common/src/main/java/com/github/chic/common/config/RedisConfig.java
+++ chic-common/src/main/java/com/github/chic/common/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Redis 配置类
@@ -16,11 +17,12 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<Object, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         RedisSerializer<Object> fastJsonRedisSerializer = new GenericFastJsonRedisSerializer();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(fastJsonRedisSerializer);
+        redisTemplate.setKeySerializer(stringRedisSerializer);
         redisTemplate.setValueSerializer(fastJsonRedisSerializer);
-        redisTemplate.setHashKeySerializer(fastJsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
         redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
         redisTemplate.setEnableTransactionSupport(true);
         redisTemplate.afterPropertiesSet();
