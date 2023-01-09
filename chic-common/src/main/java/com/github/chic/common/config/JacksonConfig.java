47d6c9a5360aefeb20f3744bd7f31ceabbdc1063
Septillions <597264070@qq.com>
2023-01-09 16:02:42 +0800

    添加 Jsckson 配置类

---
+++ chic-common/src/main/java/com/github/chic/common/config/JacksonConfig.java
@@ -0,0 +1,95 @@
+package com.github.chic.common.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Jackson 配置类
+ */
+@JsonComponent
+public class JacksonConfig {
+    /**
+     * LocalDateTime 格式
+     */
+    private static final DateTimeFormatter LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    /**
+     * LocalDate 格式
+     */
+    private static final DateTimeFormatter LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    /**
+     * LocalTime 格式
+     */
+    private static final DateTimeFormatter LOCAL_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    /**
+     * LocalDateTime 序列化配置
+     */
+    public static class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+        @Override
+        public void serialize(LocalDateTime localDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(LOCAL_DATE_TIME_FORMATTER.format(localDateTime));
+        }
+    }
+
+    /**
+     * LocalDateTime 反序列化配置
+     */
+    public static class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+        @Override
+        public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+            return LocalDateTime.parse(jsonParser.getText(), LOCAL_DATE_TIME_FORMATTER);
+        }
+    }
+
+    /**
+     * LocalDate 序列化配置
+     */
+    public static class LocalDateSerializer extends JsonSerializer<LocalDate> {
+        @Override
+        public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(LOCAL_DATE_FORMATTER.format(localDate));
+        }
+    }
+
+    /**
+     * LocalDate 反序列化配置
+     */
+    public static class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
+        @Override
+        public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+            return LocalDate.parse(jsonParser.getText(), LOCAL_DATE_FORMATTER);
+        }
+    }
+
+    /**
+     * LocalTime 序列化配置
+     */
+    public static class LocalTimeSerializer extends JsonSerializer<LocalTime> {
+        @Override
+        public void serialize(LocalTime localTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(LOCAL_TIME_FORMATTER.format(localTime));
+        }
+    }
+
+    /**
+     * LocalTime 反序列化配置
+     */
+    public static class LocalTimeDeserializer extends JsonDeserializer<LocalTime> {
+        @Override
+        public LocalTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+            return LocalTime.parse(jsonParser.getText(), LOCAL_TIME_FORMATTER);
+        }
+    }
+}
